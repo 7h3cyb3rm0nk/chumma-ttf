4ac59400c437d6b44c91ac1e36c8d22a60f7a576
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2024-06-29 12:44:02 +0530

    feat: add word replacement from config file

--- chumma_ttf/src/lib.rs
+++ chumma_ttf/src/lib.rs
@@ -1,6 +1,13 @@
 use harfbuzz_wasm::{debug, Font, Glyph, GlyphBuffer};
 use wasm_bindgen::prelude::*;
 mod dictionary;
+use dictionary::generate_words;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+lazy_static! {
+    static ref WORDS: HashMap<String, String> = generate_words();
+}
+
 #[wasm_bindgen]
 pub fn shape(
     _shape_plan: u32,
@@ -12,14 +19,21 @@ pub fn shape(
     let font = Font::from_ref(font_ref);
     let mut buffer = GlyphBuffer::from_ref(buf_ref);
 
-    let buff_u8: Vec<u8> = buffer
+    let buf_u8: Vec<u8> = buffer
         .glyphs
         .iter()
         .map(|g| g.codepoint as u8)
         .collect::<Vec<u8>>();
-    let str_buf = String::from_utf8_lossy(&buff_u8);
+    let str_buf = String::from_utf8_lossy(&buf_u8);
+    let mut result = str_buf.to_string();
+    let list: Vec<&str> = str_buf.split(" ").collect();
+    for word in list.iter().map(|x| x.to_string()) {
+        if WORDS.contains_key(&word) {
+            result = result.replace(&word, WORDS.get(&word).expect(&word));
+        }
+    }
 
-    let result = format!("{}", str_buf).replace("Hello", "Hi");
+    let result = result.replace("Hello", "Hi");
     debug(&result);
     buffer.glyphs = result
         .chars()
